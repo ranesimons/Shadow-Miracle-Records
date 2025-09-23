3b7719099459d6c453e29a9d387884751b969679
Rane Simons <ranesimons@gmail.com>
2025-09-22 22:56:04 -0500

    feat: added a new route for facebook logins

---
+++ app/facbook/page.tsx
@@ -0,0 +1,14 @@
+// pages/facebook-auth.tsx
+
+import FacebookAuthButton from "@/components/FacebookAuthButton";
+
+const FacebookAuthPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Facebook Authentication</h1>
+      <FacebookAuthButton />
+    </div>
+  );
+};
+
+export default FacebookAuthPage;
