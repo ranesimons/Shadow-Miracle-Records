7565e96077c0e9fe64586275a0f70e0bb2e20fdf
Rane Simons <ranesimons@gmail.com>
2026-04-03 18:22:12 -0500

    feat: cleaned up the landing page for luminate

--- app/page.tsx
+++ app/page.tsx
@@ -2,41 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col items-center justify-between h-screen p-1 sm:p-3">
-      <Image
-        className="Smrlogo"
-        src="/smr.png"
-        alt="Shadow Miracle Records Logo Image"
-        width={120}
-        height={120}
-      />
-      <div className="w-full">
-        <iframe className="Landing-track" width="100%" height="120" title="Alright" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1068004219&color=%23080808&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=false"></iframe>
-        <br />
-        <iframe className="Landing-track" width="100%" height="120" title="You Gonna Roll The Weed?" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1033494595&color=%23080808&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=false"></iframe>
-        <br />
-        <iframe className="Landing-track" width="100%" height="120" title="There&#x27;s Another Way To Love" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1000565353&color=%23080808&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=false"></iframe>
+    <div className="font-sans flex flex-col items-center h-screen p-4">
+      {/* 1. Add a massive bottom margin to the logo */}
+      <div className="mb-auto mt-20"> 
+        <Image
+          className="Smrlogo invert"
+          src="/smr.png"
+          alt="Shadow Miracle Records Logo Image"
+          width={120}
+          height={120}
+        />
       </div>
-      <a
-        className="
-          rounded-full
-          border border-solid border-transparent
-          transition-colors
-          flex items-center justify-center
-          bg-foreground text-background gap-2
-          hover:bg-[#383838] dark:hover:bg-[#ccc]
-          font-semibold  /* bolder */
-          text-lg sm:text-xl  /* larger font sizes */
-          h-14 sm:h-16        /* make the height bigger */
-          px-100 sm:px-100        /* more horizontal padding */
-          sm:w-auto
-        "
-        href="https://rane.streetteam.fm/join"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      Join My Community
-      </a>
+
+      {/* 2. Add a massive top margin to the button */}
+      <div className="mt-40 sm:mt-64">
+        <a
+          className="
+            rounded-full
+            border border-solid border-transparent
+            transition-colors
+            flex items-center justify-center
+            bg-foreground text-background
+            font-semibold 
+            text-lg sm:text-xl 
+            h-14 sm:h-16 
+            px-12 sm:px-20
+            w-64 sm:w-80
+          "
+          href="https://ranesimons.streetteam.fm/join"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join My Community
+        </a>
+      </div>
     </div>
   );
 }
